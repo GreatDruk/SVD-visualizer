ab69144738dc9ce33e558383ef60b0b4a32f4f45
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-09-19 23:51:33 +0300

    Make basic structure app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from dash import Dash
+
+from src.layout import layout
+from src.callbacks import get_callbacks
+
+
+def create_app():
+    app = Dash(
+        __name__,
+        title='SVD-visualizer',
+        suppress_callback_exceptions=True
+    )
+
+    app.layout = layout()
+
+    get_callbacks(app)
+
+    return app
+
+
+def main():
+    app = create_app()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
